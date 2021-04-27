d022a4ed0bf144e09a41a73838512cc6a4818c62
Juan Pablo <juanpablogast@gmail.com>
2021-04-26 21:12:25 -0400

    asas

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :jams
+  root 'home#index'
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
